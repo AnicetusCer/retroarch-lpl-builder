1b10251b61d6a3a3d21d23b998d8092a60fc9eed
AnicetusCer <33236595+AnicetusCer@users.noreply.github.com>
2022-09-17 13:41:41 +0100

    .

--- functions.sh
+++ functions.sh
@@ -254,7 +254,7 @@ check_if_rom_dir_exists()
 CheckDir=$1
   if [ ! -d "$CheckDir" ]
   then
-   printf "Notice: "${CheckDir}" not found"
+   printf "Notice: "${CheckDir}" not found\n"
    exit
   fi
 }
